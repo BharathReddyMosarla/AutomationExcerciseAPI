8942f3a04e0e74f7fce2314de241cefec16f8da8
Bharath Reddy Mosarla <166288956+BharathReddyMosarla@users.noreply.github.com>
2025-06-27 16:07:19 +0530

    Update TestCase12.java

--- src/test/java/testcases/TestCase12.java
+++ src/test/java/testcases/TestCase12.java
@@ -14,8 +14,7 @@ import routes.Routes;
 
 public class TestCase12 extends BaseClass
 {
-
-  	@Test(dependsOnMethods="testcases.TestCase11.testCreateUser")
+/*	@Test(dependsOnMethods="testcases.TestCase11.testCreateUser")
 	void testDeleteUser()
 	{ 
   		response = given()
@@ -35,7 +34,7 @@ public class TestCase12 extends BaseClass
 		assertThat(responseCode,is(200));
 		assertThat(message,is("Account deleted!"));
 	}
-  	 /*
+  	 */
 		Faker faker = new Faker();
 	    String name = faker.name().fullName();
 	    String email = faker.internet().emailAddress();
@@ -56,44 +55,45 @@ public class TestCase12 extends BaseClass
 	    String mobile_number = faker.phoneNumber().cellPhone();
 	        
 	
-	   	@Test(dependsOnMethods="testcases.TestCase11.testCreateUser")
-	void testDeleteUser()
-	{
-		given()
+@Test
+void testDeleteUser()
+{
+	given()
 		.header("Content-Type", "application/x-www-form-urlencoded")
-        .formParam("name", name)
-        .formParam("email", email)
-        .formParam("password", password)
-        .formParam("title", title)
-        .formParam("birth_date", birth_date)
-        .formParam("birth_month", birth_month)
-        .formParam("birth_year", birth_year)
-        .formParam("firstname", firstname)
-        .formParam("lastname", lastname)
-        .formParam("company", company)
-        .formParam("address1", address1)
-        .formParam("address2", address2)
-        .formParam("country", country)
-        .formParam("zipcode", zipcode)
-        .formParam("state", state)
-        .formParam("city", city)
-        .formParam("mobile_number", mobile_number)
-.when()
-	.post(Routes.POST_CREATEUSER)
-.then()
-	.statusCode(200)
-	.log().body();
+        	.formParam("name", name)
+        	.formParam("email", email)
+        	.formParam("password", password)
+        	.formParam("title", title)
+        	.formParam("birth_date", birth_date)
+        	.formParam("birth_month", birth_month)
+        	.formParam("birth_year", birth_year)
+        	.formParam("firstname", firstname)
+        	.formParam("lastname", lastname)
+        	.formParam("company", company)
+        	.formParam("address1", address1)
+        	.formParam("address2", address2)
+        	.formParam("country", country)
+        	.formParam("zipcode", zipcode)
+        	.formParam("state", state)
+        	.formParam("city", city)
+        	.formParam("mobile_number", mobile_number)
+	.when()
+		.post(Routes.POST_CREATEUSER)
+	.then()
+		.statusCode(200)
+		.log().body();
 		
-		response = given()
-				.formParam("email", email)
-	            .formParam("password", password)
-			.when()
-				.delete(Routes.DELETE_DELETEUSER)
-			.then()
-				.statusCode(200)
-				.log().body()
-				.extract().response();
-		JsonPath jsonpath = jsonpath(response);
+response = given()
+		.formParam("email", email)
+	        .formParam("password", password)
+	.when()
+		.delete(Routes.DELETE_DELETEUSER)
+	.then()
+		.statusCode(200)
+		.log().body()
+		.extract().response();
+
+	JsonPath jsonpath = jsonpath(response);
 		
 		int responseCode = jsonpath.getInt("responseCode");
 		String message = jsonpath.getString("message");
@@ -101,5 +101,5 @@ public class TestCase12 extends BaseClass
 		assertThat(responseCode,is(200));
 		assertThat(message,is("Account deleted!"));
 		
-	}*/
+	}
 }
